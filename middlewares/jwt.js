5afb7da7bc0b280a075d409e1ba70f5721d3cf24
Ashutosh Singh <singh.saahab08@gmail.com>
2024-03-29 19:20:15 +0530

    verify jwt removed

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -1,20 +1,22 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.accessToken;
+  // const token = req.cookies.accessToken;
 
-  if (!token) {
-    return res.status(401).send("You are not authenticated");
-  }
+  // if (!token) {
+  //   return res.status(401).send("You are not authenticated");
+  // }
 
-  jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
-    //       if (payload.id !== user._id.toString()) {
-    //         return res.status(403).send(" you can delete only uour account!");
-    //       }
-    if (err) return res.status(403).send("token is not valid!");
+  // jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
+  //   //       if (payload.id !== user._id.toString()) {
+  //   //         return res.status(403).send(" you can delete only uour account!");
+  //   //       }
+  //   if (err) return res.status(403).send("token is not valid!");
 
-    req.userId = payload.id;
-    req.isSeller = payload.isSeller;
-    next();
-  });
+  //   req.userId = payload.id;
+  //   req.isSeller = payload.isSeller;
+  //   next();
+  // });
+
+  next();
 };
